462c86e98a329b71226b70a17ecaa44d18115966
tqtl911@163.com <19930911cXS>
2018-09-12 11:22:30 +0800

    新增管理环境变量的相关文件-.env|.flaskenv; pip3 install python-dotenv用于进行Flask的自动发现程序实例机制。

---
+++ 2-chapter/2-1.py
@@ -0,0 +1,9 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/hello')
+def hello():
+    name = request.args.get('name', 'Flask')
+    return '<h1>Hello,%s!</h1>' % name
