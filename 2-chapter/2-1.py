afd05e7bfd9fc60680e35d580c3f29bb8e895c8a
tqtl911@163.com <19930911cXS>
2018-09-12 11:28:21 +0800

    2.2.3.2 设置监听HTTP方法

--- 2-chapter/2-1.py
+++ 2-chapter/2-1.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello():
     name = request.args.get('name', 'Flask')
     return '<h1>Hello,%s!</h1>' % name
-
+# 设置监听HTTP的方法;
 @app.route('/cxz',methods=['GET','POST'])
 def cxz():
     return '<h1>Hello,Cuixiaozhao!</h1>'
